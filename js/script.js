993677bf769e7fbd2e6e13125195c55a69d204f2
parksangseok <>
2024-09-27 03:51:23 +0900

    commit

--- js/script.js
+++ js/script.js
@@ -8,4 +8,20 @@ $(function () {
         return false;
     });
 
+    /* mobileMenu */
+    $('#header .mobileBtn').click(function () {
+        $('body').addClass('of');
+        $('#header #mobileMenuWrap').animate({
+            right: 0
+        }, 300);
+        $('.bg').show();
+    });
+    $('#header #mobileMenuWrap .closeBtn').click(function () {
+        $('body').removeClass('of');
+        $('#header #mobileMenuWrap').animate({
+            right: -3000
+        }, 300);
+        $('.bg').hide();
+    });
+
 });
